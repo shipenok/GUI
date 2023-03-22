66f07632976cd19ddf205753d386d5ea9fbe1cdf
shipenok <128097922+shipenok@users.noreply.github.com>
2023-03-22 11:33:11 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -12,13 +12,23 @@ def line_select_callback(eclick, erelease):
 
 def toggle_selector_1(event):
     print(' Key pressed.')
-    if event.key == 'enter':
+    if event.key == '1':
         if toggle_selector_1.RS.active:
             print(' RectangleSelector deactivated.')
             toggle_selector_1.RS.set_active(False)
         else:
             print(' RectangleSelector activated.')
             toggle_selector_1.RS.set_active(True)
+
+def toggle_selector_2(event):
+    print(' Key pressed.')
+    if event.key == '2':
+        if toggle_selector_2.RS.active:
+            print(' RectangleSelector deactivated.')
+            toggle_selector_2.RS.set_active(False)
+        else:
+            print(' RectangleSelector activated.')
+            toggle_selector_2.RS.set_active(True)
 
 fig, current_ax = plt.subplots()
 
@@ -29,6 +39,13 @@ toggle_selector_1.RS = RectangleSelector(current_ax, line_select_callback,
                                        spancoords='pixels',
                                        interactive=True)
 
+toggle_selector_2.RS = RectangleSelector(current_ax, line_select_callback,
+                                       drawtype='box', useblit=True,
+                                       button=[1, 3],
+                                       minspanx=5, minspany=5,
+                                       spancoords='pixels',
+                                       interactive=True)
 
 plt.connect('key_press_event', toggle_selector_1)
+plt.connect('key_press_event', toggle_selector_2)
 plt.show()
