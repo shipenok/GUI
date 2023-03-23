f3cdee3ea5626e46c3ca766a310030e9937ea806
shipenok <128097922+shipenok@users.noreply.github.com>
2023-03-23 22:17:41 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,51 +1,49 @@
 from matplotlib.widgets import RectangleSelector, EllipseSelector
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
+def choice(distance):
 
-def line_select_callback(eclick, erelease):
-    x1, y1 = eclick.xdata, eclick.ydata
-    print((x1, y1))
-    x2, y2 = erelease.xdata, erelease.ydata
-    print((x2, y2))
+    fig, current_ax = plt.subplots()
+    plt.grid()
+    current_ax.set_xlim([0,distance])
+    current_ax.set_ylim([0,distance])
 
+    def line_select_callback(eclick, erelease):
+        x1, y1 = eclick.xdata, eclick.ydata
+        x2, y2 = erelease.xdata, erelease.ydata
 
-def toggle_selector_1(event):
-    print(' Key pressed.')
-    if event.key == '1':
-        if toggle_selector_1.RS.active:
-            print(' RectangleSelector deactivated.')
-            toggle_selector_1.RS.set_active(False)
-        else:
-            print(' RectangleSelector activated.')
-            toggle_selector_1.RS.set_active(True)
+    rectangles = []
 
-def toggle_selector_2(event):
-    print(' Key pressed.')
-    if event.key == '2':
-        if toggle_selector_2.RS.active:
-            print(' RectangleSelector deactivated.')
-            toggle_selector_2.RS.set_active(False)
-        else:
-            print(' RectangleSelector activated.')
-            toggle_selector_2.RS.set_active(True)
+    def toggle_selector(event):
+        print(' Key pressed.')
+        if event.key == 'enter':
+            coordinates = toggle_selector.RS.extents
+            center = toggle_selector.RS.center
+            rect = plt.Rectangle((coordinates[0], coordinates[2]),
+                                 abs(coordinates[1]-coordinates[0]),
+                                 abs(coordinates[3]-coordinates[2]),
+                                 facecolor='g',
+                                 alpha=0.2)
+            current_ax.add_patch(rect)
+            rectangles.append(coordinates)
+            current_ax.text(center[0], center[1], str(len(rectangles)))
 
-fig, current_ax = plt.subplots()
+        if event.key == 'w':
+            toggle_selector.RS.update()
+            toggle_selector.RS.set_active(False)
+            toggle_selector.RS.set_visible(False)
+            plt.disconnect(binding_id)
 
-toggle_selector_1.RS = RectangleSelector(current_ax, line_select_callback,
-                                       drawtype='box', useblit=True,
-                                       button=[1, 3],
-                                       minspanx=5, minspany=5,
-                                       spancoords='pixels',
-                                       interactive=True)
+    toggle_selector.RS = RectangleSelector(current_ax, line_select_callback,
+                                           useblit=True,
+                                           button=[1, 3],
+                                           minspanx=5, minspany=5,
+                                           spancoords='pixels',
+                                           interactive=True,
+                                           state_modifier_keys=dict(square='shift'))
 
-toggle_selector_2.RS = RectangleSelector(current_ax, line_select_callback,
-                                       drawtype='box', useblit=True,
-                                       button=[1, 3],
-                                       minspanx=5, minspany=5,
-                                       spancoords='pixels',
-                                       interactive=True)
-
-plt.connect('key_press_event', toggle_selector_1)
-plt.connect('key_press_event', toggle_selector_2)
-plt.show()
+    binding_id = plt.connect('key_press_event', toggle_selector)
+    plt.show()
+    return rectangles
